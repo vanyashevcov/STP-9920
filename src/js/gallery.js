bf1b434e6025154def29ba4f35ed2731ed4edd01
Ivan Shevtsov <vanyashevcov@ukr.net>
2025-07-20 18:03:57 +0300

    Fixed Gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,7 +3,7 @@ import 'swiper/css';
 
 const updateGalleryPaginationIcons = () => {
   document
-    .querySelectorAll('.swiper-pagination-bullet')
+    .querySelectorAll('.gallery-swiper-pagination .swiper-pagination-bullet')
     .forEach((bullet) => {
       const useEl = bullet.querySelector('use');
       if (bullet.classList.contains('swiper-pagination-bullet-active')) {
@@ -16,19 +16,35 @@ const updateGalleryPaginationIcons = () => {
 
 const gallerySwiper = new Swiper('.gallery-swiper', {
   loop: true,
+  loopedSlides: 3,
   slidesPerView: 1,
+  slidesPerGroup: 1,
   spaceBetween: 0,
+  watchOverflow: true,
+  observer: true,
+  observeParents: true,
   breakpoints: {
     1200: {
       slidesPerView: 3,
+      slidesPerGroup: 1,
       spaceBetween: 32,
     },
   },
-
   pagination: {
     el: '.gallery-swiper-pagination',
     clickable: true,
+    dynamicBullets: false,
+    type: 'bullets',
     renderBullet: (index, className) => {
+      if (index === 0) {
+        return `
+          <span class="${className}" data-index=${index}>
+            <svg class="bullet-icon">
+              <use href="./images/sprite.svg#icon-ornament"></use>
+            </svg>
+          </span>
+        `;
+      }
       return `
         <span class="${className}" data-index=${index}>
           <svg class="bullet-icon">
@@ -38,12 +54,10 @@ const gallerySwiper = new Swiper('.gallery-swiper', {
       `;
     },
   },
-
   navigation: {
     nextEl: '.gallery-swiper-next',
     prevEl: '.gallery-swiper-prev',
   },
-
   keyboard: {
     enabled: true,
     onlyInViewport: true,
@@ -51,20 +65,6 @@ const gallerySwiper = new Swiper('.gallery-swiper', {
 });
 
 gallerySwiper.on('init', updateGalleryPaginationIcons);
-
 gallerySwiper.init();
-
 updateGalleryPaginationIcons();
-
-gallerySwiper.on('slideChange', () => {
-  updateGalleryPaginationIcons();
-  // Вимкнено автоскрол до пагінації, щоб не скролило сторінку
-  // const activeBullet = document.querySelector('.swiper-pagination-bullet-active');
-  // if (activeBullet) {
-  //   activeBullet.scrollIntoView({
-  //     behavior: 'smooth',
-  //     inline: 'center',
-  //     block: 'nearest',
-  //   });
-  // }
-});
+gallerySwiper.on('slideChange', updateGalleryPaginationIcons); 
